a30cc4e24c00c8fa943a5bf9fbb100b231bac2bb
Zakaria Fadli <zakaria1193@gmail.com>
2025-02-27 02:30:33 +0100

    curstomize lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,109 @@
+local diagnostics = {
+  "diagnostics",
+  sources = { "nvim_diagnostic" },
+  sections = { "error", "warn" },
+  symbols = { error = " ", warn = " " },
+  colored = true,
+  update_in_insert = false,
+  always_visible = true,
+}
+
+local mode = {
+  "mode",
+  fmt = function(str)
+    return "-- " .. str .. " --"
+  end,
+}
+
+local filetype = {
+  "filetype",
+  icons_enabled = true,
+  colored = true,
+  icon = nil,
+}
+
+local branch = {
+  "branch",
+  icons_enabled = true,
+  icon = "",
+}
+
+local location = {
+  "location",
+  padding = 0,
+}
+
+local filename = {
+  "filename",
+  path = 1,
+}
+
+-- cool function for progress
+local progress = function()
+  local current_line = vim.fn.line(".")
+  local total_lines = vim.fn.line("$")
+  local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
+  local line_ratio = current_line / total_lines
+  local index = math.ceil(line_ratio * #chars)
+  return chars[index]
+end
+
+local spaces = function()
+  return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+end
+
+local dirname = function()
+  return vim.fn.fnamemodify(vim.fn.getcwd(), ":~")
+end
+
+opts = {
+  options = {
+    icons_enabled = true,
+    theme = "auto",
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
+    disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
+    always_divide_middle = true,
+    globalstatus = true,
+  },
+  sections = {
+    lualine_a = { mode },
+    -- Fill filename with spaces to make it easier to read
+    lualine_b = { branch },
+    lualine_c = { diagnostics },
+    -- lualine_x = { "encoding", "fileformat", "filetype" },
+    lualine_x = { filetype, spaces, "encoding" },
+    lualine_y = { location },
+    lualine_z = { progress },
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { "filename" },
+    lualine_x = { "location" },
+    lualine_y = {},
+    lualine_z = {},
+  },
+  tabline = {
+    lualine_a = { "buffers" },
+    lualine_y = { dirname },
+    lualine_z = { filename },
+  },
+
+  extensions = {
+    "mason",
+  },
+}
+
+return {
+  {
+    "akinsho/bufferline.nvim",
+    enabled = false,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    config = function()
+      require("lualine").setup(opts)
+    end,
+  },
+}
