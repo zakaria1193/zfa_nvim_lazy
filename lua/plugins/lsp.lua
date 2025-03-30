e7999ce65be944591251229db5f7baa1f10fd040
Zakaria Fadli <zakaria1193@gmail.com>
2025-03-31 00:09:53 +0200

    Lsp diable diag virtual text

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,16 +2,20 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
-        servers = {
-          clangd = {
-            capabilities = {
+      diagnostics = {
+        virtual_text = false,
+      },
+
+      servers = {
+        clangd = {
+          capabilities = {
             textDocument = {SemanticTokens = {}},
-            },
-            on_attach = function(client, _)
-              -- IMPORTANT: Override capabilities here.
-              client.server_capabilities.semanticTokensProvider = nil
-            end,
           },
+          on_attach = function(client, _)
+            -- IMPORTANT: Override capabilities here.
+            client.server_capabilities.semanticTokensProvider = nil
+          end,
+        },
       },
     },
   },
